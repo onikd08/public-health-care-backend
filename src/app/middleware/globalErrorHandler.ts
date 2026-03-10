abc7f84100ada1d782b5024a2903c29bdac6854d
onikd08 <onikd08@gmail.com>
2026-03-10 16:20:48 +0200

    Add function to delete file from cloudinary

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -7,8 +7,9 @@ import z from "zod";
 import handleZodError from "../errorHelpers/handleZodError";
 import { IErrorResponse, IErrorSources } from "../interfaces/error.interface";
 import AppError from "../errorHelpers/AppError";
+import { deleteFileFromCloudinary } from "../../config/cloudinary.config";
 
-export const globalErrorHandler = (
+export const globalErrorHandler = async (
   error: any,
   req: Request,
   res: Response,
@@ -16,6 +17,17 @@ export const globalErrorHandler = (
 ) => {
   if (envVars.NODE_ENV === "development") {
     console.log("Error from Global Error Handler: ", error);
+  }
+
+  // cloudinary file delete
+  if (req.file) {
+    deleteFileFromCloudinary(req.file.path);
+  }
+
+  if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+    const imageUrls = req.files.map((file) => file.path);
+
+    await Promise.all(imageUrls.map((url) => deleteFileFromCloudinary(url)));
   }
 
   let statusCode: number = status.INTERNAL_SERVER_ERROR;
--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,10 +1,33 @@
 import { v2 as cloudinary } from "cloudinary";
 import envVars from "./env";
+import AppError from "../app/errorHelpers/AppError";
+import status from "http-status";
 
 cloudinary.config({
   cloud_name: envVars.CLOUDINARY.CLOUD_NAME,
   api_key: envVars.CLOUDINARY.API_KEY,
   api_secret: envVars.CLOUDINARY.API_SECRET,
 });
+
+export const deleteFileFromCloudinary = async (url: string) => {
+  try {
+    const regex = /\/v\d+\/(.+?)(?:\.[a-zA-Z0-9]+)+$/;
+
+    const match = url.match(regex);
+
+    if (match && match[1]) {
+      const publicId = match[1];
+
+      await cloudinary.uploader.destroy(publicId, { resource_type: "image" });
+    }
+  } catch (error) {
+    console.log("Error deleting file from cloudinary: ", error);
+
+    throw new AppError(
+      status.INTERNAL_SERVER_ERROR,
+      "Error deleting file from cloudinary",
+    );
+  }
+};
 
 export const cloudinaryUpload = cloudinary;
