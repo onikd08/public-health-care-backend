41ca59918b61be6143733d575473a201cb2b7939
onikd08 <onikd08@gmail.com>
2026-03-10 16:30:21 +0200

    Add function for uploading file in cloudinary

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import envVars from "./env";
 import AppError from "../app/errorHelpers/AppError";
 import status from "http-status";
@@ -8,6 +8,61 @@ cloudinary.config({
   api_key: envVars.CLOUDINARY.API_KEY,
   api_secret: envVars.CLOUDINARY.API_SECRET,
 });
+
+export const uploadFileToCloudinary = async (
+  buffer: Buffer,
+  fileName: string,
+): Promise<UploadApiResponse> => {
+  if (!buffer || !fileName) {
+    throw new AppError(
+      status.BAD_REQUEST,
+      "File buffer and File name required",
+    );
+  }
+
+  const extension = fileName.split(".").pop()?.toLowerCase();
+  const fileNameWithoutExtension = fileName
+    .split(".")
+    .slice(0, -1)
+    .join(".")
+    .toLowerCase()
+    .replace(/\s/g, "-")
+    // eslint-disable-next-line no-useless-escape
+    .replace(/[^a-z0-9\-]/g, ""); // my#file => my-file
+
+  const uniqueFileName =
+    Math.random().toString(36).substring(2) +
+    "-" +
+    Date.now() +
+    "-" +
+    fileNameWithoutExtension;
+
+  const folder = extension === "pdf" ? "pdfs" : "images";
+
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream(
+        {
+          resource_type: "auto",
+          public_id: uniqueFileName,
+          folder: `public-health-care/${folder}`,
+        },
+        (error, result) => {
+          if (error) {
+            return reject(
+              new AppError(
+                status.INTERNAL_SERVER_ERROR,
+                "Failed to upload file to cloudinary",
+              ),
+            );
+          } else {
+            resolve(result as UploadApiResponse);
+          }
+        },
+      )
+      .end(buffer);
+  });
+};
 
 export const deleteFileFromCloudinary = async (url: string) => {
   try {
